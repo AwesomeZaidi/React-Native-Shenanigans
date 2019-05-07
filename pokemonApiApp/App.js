9e558956a8d2f8b280e8ba4f7aaf86b4bcec038a
Asim Zaidi <asimzaidih@gmail.com>
2019-05-07 10:40:36 -0700

    changed app render

--- pokemonApiApp/App.js
+++ pokemonApiApp/App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
+import PokeList from './components/PokeList';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,25 +17,18 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+const App = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.pokeListHeader}>Pokemon API</Text>
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000',
   },
   welcome: {
     fontSize: 20,
@@ -46,4 +40,10 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  pokeListHeader: {
+    fontSize: 20,
+    color: '#fff'
+  }
 });
+
+export default App;
