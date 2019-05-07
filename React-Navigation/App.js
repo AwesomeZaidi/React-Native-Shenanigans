a4774e70afa2478198c039b4145f5f2bb3cc1a90
Asim Zaidi <asimzaidih@gmail.com>
2019-05-06 23:09:24 -0700

    authentication loading screen

--- React-Navigation/App.js
+++ React-Navigation/App.js
@@ -4,6 +4,7 @@ import HomeScreen from './pages/Home/'
 import DetailScreen from './pages/Detail/'
 import SignUpScreen from './pages/AuthScreens/SignUp/'
 import SignInScreen from './pages/AuthScreens/SignIn/'
+import AuthLoadingScreen from './pages/AuthScreens/AuthLoading';
 
 // createStackNavigator: provides a way for the app to transition between
 // screens where each new screen is placed on top of a stack.
@@ -24,17 +25,28 @@ const AppStack = createStackNavigator({
 
 // Idea to split the stacks  for organization, by Nicolai!
 const AuthStack = createStackNavigator({ 
-	SignUp: SignUpScreen, 
-	SignIn: SignInScreen 
+	SignIn: SignInScreen,
+	SignUp: SignUpScreen
 });
 
 // Containers are responsible for managing your app state and linking your top-level navigator to the app environment.
 // TODO: hmm what is the switchNavigator being used here for?
+// to only ever show one screen at a time. By default, it does not handle back
+// actions and it resets routes to their default state when you switch away.
+// This is the exact behavior that we want from the authentication flow: when users sign in,
+// we want to throw away the state of the authentication flow and unmount all of the screens, and when
+// we press the hardware back button we expect to not be able to go back to the authentication flow.
+// We switch between routes in the SwitchNavigator by using the navigate action.
 const App = createAppContainer(createSwitchNavigator(
 	{
+		AuthLoading: AuthLoadingScreen,
 		App: AppStack,
 		Auth: AuthStack,
-	}
+	},  {
+		// We set the initialRouteName to 'AuthLoading' because we will fetch our authentication
+		// state from persistent storage inside of that screen component.
+		initialRouteName: 'AuthLoading',
+	  }
 ));
 // Now App is the main component for React to render
 
