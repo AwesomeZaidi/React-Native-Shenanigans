1624f180df6a78dbd801ba92debcaaec96ffb448
Asim Zaidi <asimzaidih@gmail.com>
2019-05-09 11:05:04 -0700

    auth of redux store implemented

---
+++ React-Navigation/redux/actions/index.js
@@ -0,0 +1,61 @@
+// src/js/actions/index.js
+
+import { HANDLE_LOGIN, HANDLE_SIGNUP, HANDLE_LOGOUT, HANDLE_ERROR, HANDLE_ADD_RESTAURANT, HANDLE_ADD_LOCATION, HANDLE_GET_ITEMS } from "../constants/action-types";
+import axios from "axios";
+
+const baseUrl = 'https://clubs-app-backend.herokuapp.com/';
+
+export function login(loginState) {
+    return (dispatcher) => { // read more into dispatcher
+        axios.post(`${baseUrl}users/v0/login`, loginState).then((res) => {
+            dispatcher(handleLogin(res.data.user)); // THUNKED IT!
+        }).catch((err) => {
+            dispatcher(handleLogin(true));
+        });
+    };
+};
+
+export const handleLogin = (user) => {
+    return {
+        type: HANDLE_LOGIN,
+        payload: user,
+        payload_error: false
+    };
+};
+
+export function signup(signupState) {
+    return (dispatcher) => {
+        axios.post(`${baseUrl}users/v0/signup`, signupState).then((res) => {
+            console.log("res.data:", res.data);
+            dispatcher(handleSignup(res.data));
+        }).catch(console.err);
+    };
+};
+
+export const handleSignup = (user) => {
+    return {
+        type: HANDLE_SIGNUP,
+        payload: user,
+        payload_error: false
+    };
+};
+
+export const logout = () => {
+    return (dispatcher) => {
+        dispatcher(handleLogout());
+    }
+};
+
+export const handleLogout = () => {
+    return {
+        type: HANDLE_LOGOUT,
+        payload: ""
+    };
+};
+
+export const handleError = (error) => {
+    return {
+        type: HANDLE_ERROR,
+        payload: error
+    };
+};
