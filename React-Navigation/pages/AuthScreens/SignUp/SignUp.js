0ac38fa4f49444e27c298bc33af2c44a8d5b4573
Asim Zaidi <asimzaidih@gmail.com>
2019-05-13 17:42:48 -0700

    signin/signup

--- React-Navigation/pages/AuthScreens/SignUp/SignUp.js
+++ React-Navigation/pages/AuthScreens/SignUp/SignUp.js
@@ -1,181 +1,112 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-    TouchableHighlight,
-    Image,
-    Alert
-  } from 'react-native';
-  
 
-class SignUp extends Component {
-    constructor(props) {
-        super(props);
-        state = {
-            email: '',
-            password: '',
-        };
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { signUp } from "../../../redux/actions/index";
+console.log('signUp:', signUp);
+
+// ----------------------------------------------------------------------------------
+// React Native Imports
+// ----------------------------------------------------------------------------------
+import { View, Text, Button, AsyncStorage, TextInput } from 'react-native';
+
+// ----------------------------------------------------------------------------------
+// Components / Styles Imports
+// ----------------------------------------------------------------------------------
+import styles from '../styles/forms.js';
+import { placeholder } from '../../styles/variables';
+
+// ----------------------------------------------------------------------------------
+// SignUp Component Class
+// ----------------------------------------------------------------------------------
+class SignUp extends Component {    
+    // ------------------------------------------
+    // State
+    // ------------------------------------------
+    state = {
+        email: '',
+        username: '',
+        password: ''
     };
 
-    onClickListener = (viewId) => {
-        Alert.alert("Alert", "Button pressed " + viewId);
-    }
- 
+    // ------------------------------------------
+    // Navigation Options: title
+    // ------------------------------------------
     static navigationOptions = {
-		title: 'Sign up'
+        title: 'Signup',
+    };
+
+    // ------------------------------------------
+    // Update state on text change
+    // ------------------------------------------
+    onChangeText = (text, feildName) => {
+        this.setState({
+            [feildName]: text
+        });
+    };
+    // onChangeText(text, fieldName) {
+    //     this.setState({
+    //       [fieldName]: text,
+    //     })
+    // }
+    
+    // ------------------------------------------
+    // signUp redux action handler function attached to props. 
+    // ------------------------------------------ 
+    handleSubmit = async () => {
+        await this.props.signUp(this.state);
+        this.props.navigation.navigate('App');
     };
 
     render() {
+        console.log('in render');
+
+        // { this.props.user ? this.props.navigation.navigate('Home') : null }
         return (
-            <View style={styles.container}>
-                <View style={styles.inputContainer}>
-                <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
-                <TextInput style={styles.inputs}
-                    placeholder="Email"
-                    keyboardType="email-address"
-                    underlineColorAndroid='transparent'
-                    onChangeText={(email) => this.setState({email})}/>
-                </View>
-                
-                <View style={styles.inputContainer}>
-                <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
-                <TextInput style={styles.inputs}
-                    placeholder="Password"
-                    secureTextEntry={true}
-                    underlineColorAndroid='transparent'
-                    onChangeText={(password) => this.setState({password})}/>
-                </View>
-        
-                <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
-                <Text style={styles.loginText}>Login</Text>
-                </TouchableHighlight>
-        
-                <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
-                    <Text>Forgot your password?</Text>
-                </TouchableHighlight>
-        
-                <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
-                    <Text>Register</Text>
-                </TouchableHighlight>
-            </View>
+          <View style={styles.formStyle}>
+            <TextInput
+                onChangeText={(text) => this.onChangeText(text, 'email')}
+                value={this.state.email}
+                placeholder='Email Adress'
+                placeholderTextColor={placeholder}
+                style={styles.inputField}
+                autoCapitalize='none'
+                keyboardType='email-address'
+            />
+            <TextInput
+                onChangeText={(text) => this.onChangeText(text, 'username')}
+                value={this.state.username}
+                style={styles.inputField}
+                placeholder='Username'
+                autoCapitalize='none'
+                placeholderTextColor={placeholder}
+            />
+            <TextInput
+                onChangeText={(text) => this.onChangeText(text, 'password')}
+                value={this.state.password}
+                style={styles.inputField}
+                placeholder='Password'
+                placeholderTextColor={placeholder}
+            />
+            <Button
+                title="Signup"
+                buttonStyle={styles.primary_btn}
+                onPress={this.handleSubmit}
+            />
+          </View>
         );
     };
 };
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#DCDCDC',
-    },
-    inputContainer: {
-        borderBottomColor: '#F5FCFF',
-        backgroundColor: '#FFFFFF',
-        borderRadius:30,
-        borderBottomWidth: 1,
-        width:250,
-        height:45,
-        marginBottom:20,
-        flexDirection: 'row',
-        alignItems:'center'
-    },
-    inputs:{
-        height:45,
-        marginLeft:16,
-        borderBottomColor: '#FFFFFF',
-        flex:1,
-    },
-    inputIcon:{
-      width:30,
-      height:30,
-      marginLeft:15,
-      justifyContent: 'center'
-    },
-    buttonContainer: {
-      height:45,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom:20,
-      width:250,
-      borderRadius:30,
-    },
-    loginButton: {
-      backgroundColor: "#00b5ec",
-    },
-    loginText: {
-      color: 'white',
-    }
-});
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    };
+};
 
-export default SignUp;
+function mapDispatchToProps() {
+    return {
+        signUp
+    };
+};
 
-// MY OLD CODE AND STYLE
-{/* <View style={styles.container}>
-<TextInput
-    style={styles.input}
-    placeholder='Username'
-    autoCapitalize="none"
-    placeholderTextColor='white'
-    onChangeText={val => this.onChangeText('username', val)}
-/>
-<TextInput
-    style={styles.input}
-    placeholder='Password'
-    secureTextEntry={true}
-    autoCapitalize="none"
-    placeholderTextColor='white'
-    onChangeText={val => this.onChangeText('password', val)}
-/>
-<TextInput
-    style={styles.input}
-    placeholder='Email'
-    autoCapitalize="none"
-    placeholderTextColor='white'
-    onChangeText={val => this.onChangeText('email', val)}
-/>
-<TextInput
-    style={styles.input}
-    placeholder='Phone Number'
-    autoCapitalize="none"
-    placeholderTextColor='white'
-    onChangeText={val => this.onChangeText('phone_number', val)}
-/>
-<Button
-    title='Sign Up'
-    onPress={this.signUp}
-/>
-</View>
-
-const styles = StyleSheet.create({
-    input: {
-      width: 350,
-      height: 55,
-      backgroundColor: '#42A5F5',
-      margin: 10,
-      padding: 8,
-      color: 'white',
-      borderRadius: 14,
-      fontSize: 18,
-      fontWeight: '500',
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-}); */}
-// onChangeText = (key, val) => { this.setState({ [key]: val }) }
-// signUp = async () => {
-//     const { username, password, email, phone_number } = this.state
-//     try {
-//       // here place signup logic
-//       console.log('user successfully signed up!: ', success)
-//     } catch (err) {
-//       console.log('error signing up: ', err)
-//     };
-// };
+export default connect(mapStateToProps, mapDispatchToProps())(SignUp);
---
+++ React-Navigation/pages/AuthScreens/styles/forms.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+import { red, green, grey } from '../../styles/variables';
+
+const styles = StyleSheet.create({
+  appTitle: {
+    marginTop: 60,
+    textAlign: 'center',
+    fontSize: 36,
+  },
+  loginForm: {
+    marginTop: 20,
+    fontSize: 18,
+    padding: 24,
+    paddingBottom: 12,
+  },
+  inputField: {
+    fontSize: 16,
+    backgroundColor: 'white',
+    padding: 18,
+    marginTop: 6,
+    marginBottom: 6,
+    marginLeft: 12,
+    marginRight: 12,
+  },
+  helperText: {
+    marginLeft: 36,
+    marginRight: 36,
+    color: red,
+    marginBottom: 12,
+  },
+  ctaBtn: {
+    backgroundColor: green,
+    marginTop: 0,
+    padding: 12,
+    marginLeft: 36,
+    marginRight: 36,
+  },
+  otherOption: {
+    marginTop: 24,
+    display: 'flex',
+    justifyContent: 'center', 
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  otherOptionText: {
+    fontSize: 16,
+    color: grey,
+  },
+  otherBtn: {
+    fontSize: 16,
+    padding: 0,
+    marginLeft: 12,
+    color: green,
+  }
+});
+
+export default styles;
