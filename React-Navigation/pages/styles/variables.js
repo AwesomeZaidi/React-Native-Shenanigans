8de36354cb5631184ecf0f067eed6c372c08d349
Asim Zaidi <asimzaidih@gmail.com>
2019-05-14 00:39:02 -0700

    updating color vars

--- React-Navigation/pages/styles/variables.js
+++ React-Navigation/pages/styles/variables.js
@@ -1,6 +1,6 @@
 const bg = '#EBEFF3'; // light gray
 const primary = '#488aff'; // idk
-const secondary = '#32db64'; // dark purple
+const secondary = '#696969'; // dark gray
 const danger = '#fa4359'; // smooth red
 const placeholder = '#757575'; // med grey
 const light = '#f4f4f4'; // light grey
